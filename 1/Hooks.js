9ff4684a115d8551f311da2736f1d532d980e93f
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-09-04 17:22:20 -0400

    First problem

---
+++ 1/Hooks.js
@@ -0,0 +1,18 @@
+//A button that increments and adds one//
+
+import React, { useState } from "react";
+
+
+
+function Counter () {
+    const [count, setCount] = useState (0);
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click to increment!
+            </button>
+        </div>
+    )
+}
--- 1/README.md
+++ 1/README.md
@@ -1,1 +1,3 @@
 First problem: 
+
+Create a button that will add +1 to an array using hooks
