8f8f7f3c3122a5a01c6f096a6e4db88d952ff157
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-09-08 16:53:50 -0400

    State

---
+++ 1/Class.js
@@ -0,0 +1,25 @@
+// The props and state
+
+class Example extends React.Component {
+    contructor (props) {
+        super(props);
+        this.tate = {
+            count: 0
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <p>You clicked {this.state.count} times </p>
+                <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+                    Click me
+                </button>
+
+            </div>
+
+        )
+            
+        
+    }
+}
