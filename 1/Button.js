9e934ba7011ca868cf9edebed9fe5459315bc69e
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-09-08 16:54:56 -0400

    formatting

--- 1/Button.js
+++ 1/Button.js
@@ -2,17 +2,13 @@
 
 import React, { useState } from "react";
 
+function Counter() {
+  const [count, setCount] = useState(0);
 
-
-function Counter () {
-    const [count, setCount] = useState (0);
-
-    return (
-        <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click to increment!
-            </button>
-        </div>
-    )
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click to increment!</button>
+    </div>
+  );
 }
--- 1/Class.js
+++ 1/Class.js
@@ -1,25 +1,21 @@
 // The props and state
 
 class Example extends React.Component {
-    contructor (props) {
-        super(props);
-        this.tate = {
-            count: 0
-        }
-    }
+  contructor(props) {
+    super(props);
+    this.tate = {
+      count: 0,
+    };
+  }
 
-    render () {
-        return (
-            <div>
-                <p>You clicked {this.state.count} times </p>
-                <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-                    Click me
-                </button>
-
-            </div>
-
-        )
-            
-        
-    }
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times </p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+      </div>
+    );
+  }
 }
