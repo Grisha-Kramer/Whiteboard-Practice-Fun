8bb8891adc3812b00b0e31e2b03cde2aba813372
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-09-11 15:01:19 -0400

    Fibonacci

--- 2/Button.js
+++ 2/Button.js
@@ -16,8 +16,26 @@ function Example() {
     )
 }
 
+var i;
+var fib = []; // Initialize array!
+
+fib[0] = 0;
+fib[1] = 1;
+for (i = 2; i <= 10; i++) {
+  // Next fibonacci number = previous + one before previous
+  // Translated to JavaScript:
+  fib[i] = fib[i - 2] + fib[i - 1];
+  console.log(fib[i]);
+}
+
 
 //Need to program logic for fibonacci sequence
+//The Rule is xn = xn−1 + xn−2
+// The Fibonacci Sequence is the series of numbers:
+
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+//The next number is found by adding up the two numbers before it:
 
 var i;
 for (i = 0; i < cars.length; i++) {
